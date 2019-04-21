745470df2bdf44558bb342a4d612c094ff6224a7
Marco Tatone <m.tatone@hotmail.com>
2019-04-21 12:43:48 -0400

    added comments routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,12 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  resources :posts
+  resources :posts do 
+    resources :comments, :except => [:update, :detroy]
+  end
 
+  resources :comments, :only => [:update, :destroy]
   #directs the root directory to the posts root
   root "posts#index"
 
+  root "comments#index"
 end
